f7a7c6c15586255042ec3d49cb1096fb2cc20fb7
Demar Porter <demarporter@Demar-MBP.attlocal.net>
2023-07-30 16:38:22 -0400

    added header avatar image input field and button and logo and margins and padding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
+import { useState } from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import videos from "./data/videos.json";
+import videoDetails from "./data/video-details.json"
+
 
 function App() {
+
+  const [current, setCurrentVideo] = useState(videoDetails[0]);
+
+  const handleNextvideo = nextVideo => {
+    const foundVideo = videoDetails.find(video => nextVideo.id ===video.id)
+    setCurrentVideo(foundVideo);
+  }
+
+    const filterVideos = videos.filter(video => video.id )
   return (
+    <>
     <div className="App">
+    <Header />  
       
     </div>
+    </>
   );
 }
 
