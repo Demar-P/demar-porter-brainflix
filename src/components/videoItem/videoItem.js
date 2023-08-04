4a26e5d718aa0b29d2e9c3c5de445e320238091d
Demar-P <demarpmusic@gmail.com>
2023-08-04 15:25:22 -0400

    updated video to populate player with new video once clicked

--- src/components/videoItem/videoItem.js
+++ src/components/videoItem/videoItem.js
@@ -1,18 +1,19 @@
 import './videoItem.scss'
 
-function videoItem({title,id, handleNextVideo, image, channel}){
-    return(
+function videoItem({ title, id, handleNextVideo, image, channel }) {
+    console.log('ID in video is', id)
+    return (
         <>
-        <hr />
-        <div className="new-video" onClick={()=> handleNextVideo()}>
-            <div className="new-video__img">
-            <img className="next-video__img--item" src={image} alt="" />
+            <hr />
+            <div className="new-video" onClick={() => handleNextVideo(id)}>
+                <div className="new-video__img">
+                    <img className="next-video__img--item" src={image} alt="" />
+                </div>
+                <div className="next-video__text">
+                    <p className='next-video__title'>{title}</p>
+                    <p className='next-video__channel'>{channel}</p>
+                </div>
             </div>
-            <div className="next-video__text">
-                <p className='next-video__title'>{title}</p>
-                <p className='next-video__channel'>{channel}</p>
-            </div>
-        </div>
         </>
     )
 }
