158667263b52d8cd34f369abdb9c3e2b4c3e7742
Demar-P <demarpmusic@gmail.com>
2023-08-05 20:40:34 -0400

    created homepage and upload page and routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import './App.scss';
-import Header from './components/Header/Header';
-import videos from "./data/videos.json";
-import videoDetails from "./data/video-details.json"
-import VideoSection from "./components/VideoSection/VideoSection";
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import UploadPage from "./pages/UploadPage/UploadPage";
+import Header from "./components/Header/Header";
 
 
 function App() {
 
-  const [currentVideo, setCurrentVideo] = useState(videoDetails[0]);
-
-  function handleNextvideo(nextVideoID) {
-    const foundVideo = videoDetails.find((video) => {
-      return video.id === nextVideoID
-    })
-    console.log('this is the next video on app.js ', nextVideoID, foundVideo, 'this is the foundVideo')
-
-    setCurrentVideo(foundVideo);
-  }
-
-  const filterVideos = videos.filter((video) => {
-    return currentVideo && video.id !== currentVideo.id;
-  })
-
-
   return (
     <>
-      <Header />
-      <VideoSection currentVideo={currentVideo} filterVideos={filterVideos} handleNextVideo={handleNextvideo} />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload-page" element={<UploadPage />} />
+        </Routes>
 
+      </BrowserRouter>
     </>
-  );
-}
+  )
+};
 
-export default App; 
+
+
+export default App;
